eda9e943123b4ad00daceab908e3f59fa1addfb0
Machenike <ulisesupc@gmail.com>
2026-02-15 22:50:07 -0700

    Arbol Binario Diagnostico

---
+++ ArbolBinario.java
@@ -0,0 +1,30 @@
+public class ArbolBinario {
+    Nodo raiz;
+
+    public ArbolBinario() {
+        this.raiz = null;
+    }
+    public void insertar(int valor) {
+        raiz = insertarRecursivo(raiz, valor);
+    }
+    private Nodo insertarRecursivo(Nodo actual, int valor) {
+        if (actual == null) {
+            return new Nodo(valor);
+        }
+
+        if (valor < actual.valor) {
+            actual.izquierdo = insertarRecursivo(actual.izquierdo, valor);
+        } else if (valor > actual.valor) {
+            actual.derecho = insertarRecursivo(actual.derecho, valor);
+        }
+
+        return actual;
+    }
+    public void recorrerInorden(Nodo nodo) {
+        if (nodo != null) {
+            recorrerInorden(nodo.izquierdo);
+            System.out.print(nodo.valor + " ");
+            recorrerInorden(nodo.derecho);
+        }
+    }
+}
